67933f2be6f401fa9359c6dd31f65c84d78a6a82
Dunja Petrovic <32639804+DunjaPet@users.noreply.github.com>
2020-12-10 12:55:55 +0100

    added exceptions handling

--- search_substring_for_pattern.py
+++ search_substring_for_pattern.py
@@ -1,9 +1,11 @@
 import copy
 from collections import Counter 
 
-
+# real sequence
 #txt="ITAGDKIWAIIAGDPEILDRAVQMISPDHDELRVAVPLGFNNLPAHSTIEVYVLEKITLTLMFRGLGVTKPLQTRGAAGRGLITDSYKTAEIIVSRAVGYTITKSGKHPPGSPQIACFMKNMRFQKARTHVLGSALNDGFFIDLAADIKALVEPQVAPCSTAKQASRLSAPEKSPTTQTPTGRGLIQAIRVPERVKLQLTIVVVVGGAPYPTCTSTAGGLAAFRALVIMSVPGLVVLSISKCTQGPKGIANDPSLTYKPAGAERGKIVHGSLWVSLRIKTGAANEKVWEVWGIMAAQPITSEKTIMQQIKRVPLAGLQSLVLVGTSIVDIGVSYYALLMGKLTVEENQTELVFAEAPSLQLIDFDVRHIYRATIDGVFMGGVDIKDGGMEELPDPKTKAGLSAKDNVARALMECTNDVSPYSDIQIPLRSDSLAAYADLKISLETIRAHYNIGVGVALARELSLYNFLAKAGLEKEVQNTMTEQFVHLYESRKNDLYKLVRDYGSLEIPEKGYLVVYMQDAAPLDGSRANMMIPGGGTINVSQRLVSIVAYCNSYALAPNSAAVGALGVANIQNLSLLNLHLIEGKIGEYKQLVLYTGQTLTQAKKMVERFYASQSDLVEAIFIGAQYSIVLFYPPLQGKPFRWPFISCEELASPPNHCHTRSRYLVGLAYYVSQNSPAHTDEFVFEPKDKYFVIALELCSMKSITALRKTDMMYLDVKHQLSNFEDSIGLVLPLSFTHFNAVMHLGKRVSRIATIRAHALEFGDIAFISSAQPFDWAGKSAKFSAPDKQLPYRELSANR"
-txt="DEAAACEBEAADBBAABBBBCECAACDDEECEBCDADBDC"
+# test sequence below
+txt="HDELRVAVPLGFNNLPAHSTIEVYVLE"
+
 
 hashTB = []
 lens=[]
@@ -12,13 +14,12 @@ lens=[]
 #--------------- functions----------------
 
 def checkrow(txt,ptn):
-	for j in range(len(txt)-len(ptn)+1):				# second dimension for hashTB
+	for j in range(len(txt)-len(ptn)+1):						# second dimension for hashTB
 		hashTB.append([])	
-		ptnC = copy.deepcopy(ptn)				# copy ptn in every itteration		
-		for i in range(j,len(txt)):    # itterating trough txt  (one dimension)
+		ptnC = copy.deepcopy(ptn)						# copy ptn in every itteration		
+		for i in range(j,len(txt)):    						# itterating trough txt  (one dimension)
 					
-					
-			if txt[i] in ptnC:   				# check i-th letter in txt if is in ptn 
+			if txt[i] in ptnC:   						# check i-th letter in txt if is in ptn 
 							
 				ptnC.remove(txt[i])
 				hashTB[j].append(1)
@@ -27,32 +28,36 @@ def checkrow(txt,ptn):
 						break		
 			else:
 				hashTB[j].append(0)
-
-
 			
 		if sum(hashTB[j])==len(ptn):
 			lens.append(len(hashTB[j]))
+
+	return min(lens)
+
+
+
+
+def excess_letters (txt):
+	
+		ptn=[]
+		freqs = Counter(txt)
+		if	len(txt)==800:
 		
-
-		
-	return(min(lens))
-
-
-
-
-def excessletters (txt):
-	ptn=[]
-	freqs = Counter(txt)
-	for k,v in freqs.items():
-		if v > 8:
-			value = v - 8
-			ptn.append([k]*value)
-			
+			for k,v in freqs.items():
+				if v > 40:		
+					value = v - 40
+					ptn.append([k]*value)
+							
+				else:
+					continue
+					
+			flat_ptn = [item for sublist in ptn for item in sublist]
+				
+			return flat_ptn
 		else:
-			continue
-		flat_ptn = [item for sublist in ptn for item in sublist]
-	return(flat_ptn)
+			print ("bad sequence")
+			
 
-ptn = excessletters(txt)
-print (ptn)
+ptn = excess_letters(txt)
+
 print (checkrow(txt,ptn))
